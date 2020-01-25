baec83dc4d24709120ee1cf62743f8fbd0d72f97
timur <timurkim772@gmail.com>
2020-01-25 10:37:15 +0300

    Нахождение суммы чисел

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,17 +7,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
-            
-            if (primes.Contains(n))
-            {
-                Console.WriteLine("Boruto");
-            }
-            else
-            {
-                Console.WriteLine("Saske");
-            } 
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b + c);
         }
     }
 }
