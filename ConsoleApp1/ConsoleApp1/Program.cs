48b337fb752b9c69a106d3beb9eb5ecc40406dc2
timur <timurkim772@gmail.com>
2020-02-08 13:57:54 +0300

    выведение 5 из чисел

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,22 +7,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int a = n % 10;
-            int b = (n / 10) % 10;
-            int c = (n / 100) % 10;
-            int d = (n / 1000) % 10;
-            if (n < 10000 && n > 999)
-            {
-                Console.WriteLine(a + b + c + d);
-                Console.WriteLine($"{d}{c}{b}{a}");
-                Console.WriteLine($"{d}{a}{c}{b}");
-                Console.WriteLine($"{a}{c}{b}{d}");
-            }
-            else 
-            {
-                Console.WriteLine("потрачено");
-            } 
+            int n =
+            int.Parse(Console.ReadLine());
+            int m =
+            int.Parse(Console.ReadLine());
+            Console.WriteLine(Enumerable.Range(n+1, m-n-1).Select(x => x%5).Where(x => x==0).Count());
 
         }
     }
