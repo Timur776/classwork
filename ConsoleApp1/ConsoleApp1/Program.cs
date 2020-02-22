482fc20d063d81e50ae5f669af97bfcb04b18a59
timur <timurkim772@gmail.com>
2020-02-22 14:01:31 +0300

    обмен переменных местами

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,21 +7,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int n = 10;
-            double S = 1;
-            for (int i = 2; ; i++)
-            {
-                double k = 1.0 / i * Math.Pow(-1, i);
-                if (Math.Abs(k) < 0.001)
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+                if (n > m)
                 {
-                    break;
+                n += m;
+                m = n - m;
+                n -= m;
                 }
-                else
-                {
-                    S += k;
-                }
-            }
-            Console.WriteLine(S);
+            Console.WriteLine(n);
+            Console.WriteLine(m);
         }
     }
 }
