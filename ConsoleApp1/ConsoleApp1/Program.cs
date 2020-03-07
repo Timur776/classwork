7e67b12e8dcf7900a09f3ec3987b984042d44816
timur <timurkim772@gmail.com>
2020-03-07 11:05:21 +0300

    вывод чисел от одного до n

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,27 +7,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            switch (n)
+            int n =int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
             {
-                case 1:
-                case 2:
-                case 3:
-                    Console.WriteLine(n * 10);
-                    break;
-                case 4:
-                case 5:
-                case 6:
-                    Console.WriteLine(n * 100);
-                    break;
-                case 7:
-                case 8:
-                case 9:
-                    Console.WriteLine(n * 1000);
-                    break;
-                default:
-                    Console.WriteLine("error");
-                    break;
+                Console.WriteLine(i + 1);
             }
         } 
     }
