2675d20fbfc4e512fe672c70e0ef4be6a5cb6e1c
timur <timurkim772@gmail.com>
2020-03-07 11:15:11 +0300

    сумма последовательных чисел

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,11 +7,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            long S = 0;
             int n =int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(i + 1);
+                S += i + 1;
             }
+            Console.WriteLine(S);
         } 
     }
 }
